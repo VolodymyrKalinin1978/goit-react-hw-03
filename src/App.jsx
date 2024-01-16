767ee22eee37f411f23dc8ae0b94f31e10df2d98
VolodymyrKalinin1978 <122758451+VolodymyrKalinin1978@users.noreply.github.com>
2024-01-16 22:43:34 +0200

    initialization
    start Dev

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import 'modern-normalize'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Heloo</h1>
+    </div>
+  )
+}
+
+export default App
